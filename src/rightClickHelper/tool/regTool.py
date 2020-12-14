ca0532f989c6eadf1b26608be664e41155b0a239
yijie4188 <yijie4188@gmail.com>
2020-12-14 22:13:14 +0800

    [feat] (tool) 完善了regTool工具类，并创建了其测试类，用于单元测试

--- src/rightClickHelper/tool/regTool.py
+++ src/rightClickHelper/tool/regTool.py
@@ -2,46 +2,153 @@
 # -*- coding: utf-8 -*-
 import json, winreg
 from enum import Enum
+from src.rightClickHelper.tool.systemTool import SystemTool
 
 class CommandFlag(Enum):
     NONE = 0X00000000
     HIDE = 0X00000008
 
 class RegEnv(Enum):
-    HKEY_CLASSES_ROOT = winreg.HKEY_CLASSES_ROOT
-
-class RegTool:
-    def __init__(self):
-        pass
+    # https://docs.microsoft.com/en-us/troubleshoot/windows-server/performance/windows-registry-advanced-users
+    HKEY_CLASSES_ROOT    = winreg.HKEY_CLASSES_ROOT     # Contains the root of the configuration information for the user who is currently logged on.
+                                                        # The user's folders, screen colors, and Control Panel settings are stored here.
+                                                        # This information is associated with the user's profile.
+                                                        # This key is sometimes abbreviated as HKCU.
+    HKEY_CURRENT_USER    = winreg.HKEY_CURRENT_USER     # Contains all the actively loaded user profiles on the computer.
+                                                        # HKEY_CURRENT_USER is a subkey of HKEY_USERS.
+                                                        # HKEY_USERS is sometimes abbreviated as HKU.
+    HKEY_LOCAL_MACHINE   = winreg.HKEY_LOCAL_MACHINE    # Contains configuration information particular to the computer (for any user).
+                                                        # This key is sometimes abbreviated as HKLM.
+    HKEY_USERS           = winreg.HKEY_USERS            # Is a subkey of HKEY_LOCAL_MACHINE\Software.
+                                                        # The information that is stored here makes sure that the correct program opens when you open a file by using Windows Explorer.
+                                                        # This key is sometimes abbreviated as HKCR.
+                                                        # Starting with Windows 2000, this information is stored under both the HKEY_LOCAL_MACHINE and HKEY_CURRENT_USER keys.
+                                                        # The HKEY_LOCAL_MACHINE\Software\Classes key contains default settings that can apply to all users on the local computer.
+                                                        # The HKEY_CURRENT_USER\Software\Classes key contains settings that override the default settings and apply only to the interactive user.
+                                                        # The HKEY_CLASSES_ROOT key provides a view of the registry that merges the information from these two sources.
+                                                        # HKEY_CLASSES_ROOT also provides this merged view for programs that are designed for earlier versions of Windows.
+                                                        # To change the settings for the interactive user, changes must be made under HKEY_CURRENT_USER\Software\Classes instead of under HKEY_CLASSES_ROOT.
+                                                        # To change the default settings, changes must be made under HKEY_LOCAL_MACHINE\Software\Classes.
+                                                        # If you write keys to a key under HKEY_CLASSES_ROOT, the system stores the information under HKEY_LOCAL_MACHINE\Software\Classes.
+                                                        # If you write values to a key under HKEY_CLASSES_ROOT, and the key already exists under HKEY_CURRENT_USER\Software\Classes,
+                                                        # the system will store the information there instead of under HKEY_LOCAL_MACHINE\Software\Classes.
+    HKEY_CURRENT_CONFIG  = winreg.HKEY_CURRENT_CONFIG   # Contains information about the hardware profile that is used by the local computer at system startup.
 
     @staticmethod
-    def listKey(env: RegEnv, path: str) -> list:
-        regKey = winreg.OpenKey(env.value, path)
-        regCount = winreg.QueryInfoKey(regKey)[0]
-        regKeys = []
-        for i in range(int(regCount)):
-            name = winreg.EnumKey(regKey, i)
-            regKeys.append(name)
-        winreg.CloseKey(regKey)
+    def find(val):
+        for item in RegEnv:
+            if item.value == val:
+                return item
+        raise ValueError('RefEnv not found.')
+
+class RegType(Enum):
+    # https://docs.microsoft.com/en-us/windows/win32/sysinfo/registry-value-types
+    REG_BINARY              = winreg.REG_BINARY                 # 任何形式的二进制数据.
+    REG_DWORD               = winreg.REG_DWORD                  # 一个32位数字.
+    REG_DWORD_LITTLE_ENDIAN = winreg.REG_DWORD_LITTLE_ENDIAN    # 小尾数格式的32位数字.Windows被设计为在小端计算机体系结构上运行.因此,此值在Windows头文件中定义为REG_DWORD.
+    REG_DWORD_BIG_ENDIAN    = winreg.REG_DWORD_BIG_ENDIAN       # big-endian格式的32位数字.一些UNIX系统支持big-endian体系结构.
+    REG_EXPAND_SZ           = winreg.REG_EXPAND_SZ              # 以空值结尾的字符串,其中包含对环境变量（例如,"％PATH％"）的未扩展引用.
+                                                                # 根据您使用的是Unicode还是ANSI函数,它将是Unicode或ANSI字符串.要扩展环境变量引用,请使用ExpandEnvironmentStrings函数.
+    REG_LINK                = winreg.REG_LINK                   # 一个以空值结尾的Unicode字符串,其中包含符号链接的目标路径,
+                                                                # 该符号链接是通过使用REG_OPTION_CREATE_LINK调用RegCreateKeyEx函数创建的.
+    REG_MULTI_SZ            = winreg.REG_MULTI_SZ               # 由空字符串(\0)终止的以null终止的字符串序列.
+    REG_NONE                = winreg.REG_NONE                   # 没有定义的值类型.
+    REG_QWORD               = winreg.REG_QWORD                  # 一个64位数字.
+    REG_QWORD_LITTLE_ENDIAN = winreg.REG_QWORD_LITTLE_ENDIAN    # 小尾数格式的64位数字.
+                                                                # Windows被设计为在小端计算机体系结构上运行.因此,此值在Windows头文件中定义为REG_QWORD.
+    REG_SZ                  = winreg.REG_SZ                     # 空终止的字符串.根据您使用的是Unicode还是ANSI函数,这将是Unicode或ANSI字符串.
+
+    @staticmethod
+    def find(val):
+        for item in RegType:
+            if item.value == val:
+                return item
+        raise ValueError('RegType not found.')
+
+class RegVal:
+    def __init__(self, _type, val):
+        self.type = None
+        for regType in RegType:
+            if regType.value == _type:
+                self.type = regType
+        if self.type is None:
+            self.type = RegType.REG_NONE
+        self.val = val
+
+class RegTool:
+    @staticmethod
+    def pathExist(
+        env: RegEnv, path: str
+    ) -> bool:
+        try:
+            winreg.CloseKey(
+                winreg.OpenKey(env.value, path)
+            )
+            return True
+        except: return False
+
+    @staticmethod
+    def keyExist(
+        env: RegEnv, path: str, key: str
+    ) -> bool:
+        return RegTool.pathExist(env, path + '\\' + key)
+
+    @staticmethod
+    def createPath(
+        env: RegEnv, path: str
+    ):
+        if path == '': raise ValueError('Path cannot be empty.')
+        if RegTool.pathExist(env, path):
+            return winreg.OpenKey(env.value, path)
+
+        return winreg.CreateKey(env.value, path)
+
+    @staticmethod
+    def createKey(
+        env: RegEnv, path: str, key: str
+    ):
+        if path == '' or key == '':
+            raise ValueError('Path cannot be empty.')
+        if RegTool.keyExist(env, path, key):
+            return winreg.OpenKey(env.value, path + '\\' + key)
+
+        return RegTool.createPath(env, path + '\\' + key)
+
+    @staticmethod
+    def delKey(
+        env: RegEnv, path: str, key: str = ''
+    ):
+        if path == '' or not RegTool.pathExist(env, path):
+            raise ValueError('Path not found.')
+        if key != '' and not RegTool.keyExist(env, path, key):
+            raise ValueError('Key not found.')
+
+        def reduceDel(path):
+            subKeys = RegTool.listKey(env, path)
+            for subKey in subKeys:
+                reduceDel(path + '\\' + subKey)
+            winreg.DeleteKey(env.value, path)
+        reduceDel(path)
+
+    @staticmethod
+    def listKey(
+        env: RegEnv, path: str
+    ) -> list:
+        with winreg.OpenKey(env.value, path) as regKey:
+            regCount = winreg.QueryInfoKey(regKey)[0]
+            regKeys = []
+            for i in range(int(regCount)):
+                name = winreg.EnumKey(regKey, i)
+                regKeys.append(name)
         return regKeys
 
     @staticmethod
-    def listVal(env: RegEnv, path: str) -> dict:
-        regKey = winreg.OpenKey(env.value, path)
-        i = 0
-        regVals = {}
-        try:
-            while True:
-                name, val, type = winreg.EnumValue(regKey, i)
-                regVals[name] = val
-                i += 1
-        finally:
-            winreg.CloseKey(regKey)
-            return regVals
-
-    @staticmethod
-    def recursion(env: RegEnv, startPath: str, depth: int = 1000) -> {}:
+    def recursion(
+        env: RegEnv, startPath: str, depth: int = 1000
+    ) -> {}:
         if depth == 0: return {}
+        if startPath == '' or not RegTool.pathExist(env, startPath):
+            raise ValueError('Path not found.')
 
         originData = {}
         originList = RegTool.listKey(env, startPath)
@@ -49,9 +156,107 @@ class RegTool:
         for item in originList:
             originData[item] = RegTool.recursion(env, startPath + '\\' + item, depth - 1)
 
-        originData['__path__'] = (env, startPath)
+        originData['__path__'] = (env.value, startPath)
         originData['__val__'] = RegTool.listVal(env, startPath)
         return originData
+
+    @staticmethod
+    def writeKeyByStr(regDataStr: str):
+        return RegTool.writeKey(
+            json.loads(regDataStr)
+        )
+
+    @staticmethod
+    def writeKey(regData: dict):
+        regEnvVal, path = regData['__path__']
+        regEnv = RegEnv.find(regEnvVal)
+        if not RegTool.pathExist(regEnv, path):
+            RegTool.createPath(regEnv, path)
+
+        for key, regDataChild in regData.items():
+            if key[:2] != '__':
+                RegTool.writeKey(regDataChild)
+        for valueName, (valueContent, valueTypeVal) in regData['__val__'].items():
+            RegTool.setVal(
+                regEnv, path, valueName, valueContent
+                , RegType.find(valueTypeVal)
+            )
+
+    @staticmethod
+    def listVal(
+        env: RegEnv, path: str
+    ) -> dict:
+        with winreg.OpenKey(env.value, path) as regKey:
+            i = 0
+            regVals = {}
+            try:
+                while True:
+                    name, val, type = winreg.EnumValue(regKey, i)
+                    regVals[name] = (val, type)
+                    i += 1
+            finally:
+                return regVals
+
+    @staticmethod
+    def setVal(
+        env: RegEnv, path: str,
+        valueName: str, valueContent: str, regType: RegType = RegType.REG_SZ
+    ):
+        if not SystemTool.isAdmin():
+            raise PermissionError('Not started with administrator rights.')
+
+        with winreg.OpenKey(
+            env.value, path, access=winreg.KEY_SET_VALUE
+        ) as regKey:
+            winreg.SetValueEx(regKey, valueName, 0, regType.value, valueContent)
+
+    @staticmethod
+    def delVal(
+        env: RegEnv, path: str, valueName: str
+    ):
+        if not SystemTool.isAdmin():
+            raise PermissionError('Not started with administrator rights.')
+
+        with winreg.OpenKey(
+            env.value, path, access=winreg.KEY_SET_VALUE
+        ) as regKey:
+            winreg.DeleteValue(regKey, valueName)
+
+    @staticmethod
+    def getVal(
+        env: RegEnv, path: str, valueName: str
+    ) -> RegVal:
+        if not SystemTool.isAdmin():
+            raise PermissionError('Not started with administrator rights.')
+
+        with winreg.OpenKey(
+            env.value, path, access=winreg.KEY_QUERY_VALUE
+        ) as regKey:
+            try:
+                val, _type = winreg.QueryValueEx(regKey, valueName)
+                return RegVal(_type, val)
+            except:
+                raise FileNotFoundError('Reg val not found.')
+
+    @staticmethod
+    def mvKey(
+        source: (RegEnv, str),
+        target: (RegEnv, str)
+    ):
+        def replacePath(regData: {}):
+            regData['__path__'] = (
+                target[0].value,
+                regData['__path__'][1].replace(source[1], target[1])
+            )
+            for key, regDataChild in regData.items():
+                if key[:2] != '__':
+                    replacePath(regDataChild)
+        try:
+            sourceRegData = RegTool.recursion(*source)
+            replacePath(sourceRegData)
+            RegTool.writeKey(sourceRegData)
+        except Exception as e: raise e
+        RegTool.delKey(*source)
 
 class MenuItem:
     def __init__(self, name: str, regData: dict):
@@ -63,28 +268,37 @@ class MenuItem:
             '__val__': {}
         }).get('__val__', {})
 
-        self.title      = regDataVal.get('', '')
+        self.title      = regDataVal.get('', [''])[0]
         # type: str
-        self.icon       = regDataVal.get('Icon', '')
+        self.icon       = regDataVal.get('Icon', [''])[0]
         # type: str
-        self.command    = commandVal.get('', '')
+        self.command    = commandVal.get('', [''])[0]
         # type: str
 
-        self.isPackage  = regDataVal.get('SubCommands', False) == ''
+        self.isPackage  = regDataVal.get('SubCommands', [False])[0] == ''
         # type: bool
         # 二级菜单
-        self.isHide     = not (regDataVal.get('CommandFlags', CommandFlag.NONE.value) == CommandFlag.HIDE.value)
+        self.isHide     = not (regDataVal.get('CommandFlags', [CommandFlag.NONE.value])[0] == CommandFlag.HIDE.value)
         # type: bool
         # 隐藏
-        self.isShift    = regDataVal.get('Extended', False) == ''
+        self.isShift    = regDataVal.get('Extended', [False])[0] == ''
         # type: bool
         # 按下shift时
-        self.isExplorer = regDataVal.get('OnlyInBrowserWindow', False) == ''
+        self.isExplorer = regDataVal.get('OnlyInBrowserWindow', [False])[0] == ''
         # type: bool
         # 文件浏览器中
-        self.isNotWorkingDir = regDataVal.get('NoWorkingDirectory', False) == ''
+        self.isNotWorkingDir = regDataVal.get('NoWorkingDirectory', [False])[0] == ''
         # type: bool
         # 不以当前目录为打开的工作目录
+
+    @staticmethod
+    def createMenuItem(
+        env: RegEnv, startPath: str
+    ):
+        pass
+
+    def saveToReg(self):
+        pass
 
     @property
     def children(self) -> []:
@@ -92,8 +306,11 @@ class MenuItem:
             return []
 
         returnChildren = []
-        env, path = self.regData.get('__path__')
-        regDataTree = RegTool.recursion(env, path + '\\shell', 3)
+        envVal, path = self.regData.get('__path__')
+
+        regDataTree = RegTool.recursion(
+            RegEnv.find(envVal), path + '\\shell', 3
+        )
         for key, val in regDataTree.items():
             if key[:2] != '__':
                 returnChildren.append(
---
+++ test/TestRegTool.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import json, unittest
+
+from src.rightClickHelper.tool.regTool import RegTool, RegEnv, RegType
+
+class TestRegTool(unittest.TestCase):
+    def test_setVal(self):
+        testVal = {
+            'name': 'test',
+            'content': 'test2'
+        }
+        RegTool.setVal(
+            RegEnv.HKEY_CLASSES_ROOT, r'\Directory\shell\AnyCode',
+            testVal['name'], testVal['content'], RegType.REG_SZ
+        )
+        addRegVal = RegTool.getVal(
+            RegEnv.HKEY_CLASSES_ROOT, r'\Directory\shell\AnyCode', testVal['name']
+        )
+        self.assertEqual(
+            addRegVal.type, RegType.REG_SZ
+            , 'Value type setting error'
+        )
+        self.assertEqual(
+            addRegVal.val, testVal['content']
+            , 'Value content setting error'
+        )
+        RegTool.delVal(
+            RegEnv.HKEY_CLASSES_ROOT, r'\Directory\shell\AnyCode', testVal['name']
+        )
+        try:
+            RegTool.getVal(
+                RegEnv.HKEY_CLASSES_ROOT, r'\Directory\shell\AnyCode', testVal['name']
+            )
+        except Exception as regValNotFoundError:
+            self.assertEqual(
+                isinstance(regValNotFoundError, FileNotFoundError), True
+                , 'Registry value deletion failed'
+            )
+            self.assertEqual(
+                regValNotFoundError.__str__(), 'Reg val not found.'
+                , 'Registry value deletion failed'
+            )
+
+    def test_createKey(self):
+        RegTool.createKey(
+            RegEnv.HKEY_CLASSES_ROOT, r'*\shell\wow\foo', 'bar'
+        )
+        self.assertEqual('wow' in RegTool.listKey(
+            RegEnv.HKEY_CLASSES_ROOT, r'*\shell'
+        ), True, 'Path create failed')
+        self.assertEqual('foo' in RegTool.listKey(
+            RegEnv.HKEY_CLASSES_ROOT, r'*\shell\wow'
+        ), True, 'Path create failed')
+        self.assertEqual('bar' in RegTool.listKey(
+            RegEnv.HKEY_CLASSES_ROOT, r'*\shell\wow\foo'
+        ), True, 'Key create failed')
+
+        RegTool.delKey(
+            RegEnv.HKEY_CLASSES_ROOT, r'*\shell\wow\foo', 'bar'
+        )
+        self.assertEqual('bar' in RegTool.listKey(
+            RegEnv.HKEY_CLASSES_ROOT, r'*\shell\wow\foo'
+        ), False, 'Key delete failed')
+
+        RegTool.delKey(
+            RegEnv.HKEY_CLASSES_ROOT, r'*\shell\wow'
+        )
+        self.assertEqual('wow' in RegTool.listKey(
+            RegEnv.HKEY_CLASSES_ROOT, r'*\shell'
+        ), False, 'path delete failed')
+
+    def test_writeRegData(self):
+        writeData = {
+            '__path__': (RegEnv.HKEY_CLASSES_ROOT.value, r'*\shell\testRegData'),
+            '__val__': {
+                '': ('test reg data write', RegType.REG_SZ.value)
+            }
+            , 'child-0': {
+                '__path__': (RegEnv.HKEY_CLASSES_ROOT.value, r'*\shell\testRegData\child-0'),
+                '__val__': {
+                    '': ('test reg data child-0 write', RegType.REG_SZ.value)
+                }
+                , 'child-0-0': {
+                    '__path__': (RegEnv.HKEY_CLASSES_ROOT.value, r'*\shell\testRegData\child-0\child-0-0'),
+                    '__val__': {
+                        '': ('test reg data child-0-0 write', RegType.REG_SZ.value)
+                    }
+                }
+            }
+            , 'child-1': {
+                '__path__': (RegEnv.HKEY_CLASSES_ROOT.value, r'*\shell\testRegData\child-1'),
+                '__val__': {
+                    '': ('test reg data child-1 write', RegType.REG_SZ.value)
+                }
+            }
+        }
+
+        RegTool.writeKey(writeData)
+        self.assertEqual(
+            RegTool.recursion(
+                *(RegEnv.HKEY_CLASSES_ROOT, r'*\shell')
+            )['testRegData'] == writeData, True
+            , 'Data write failed'
+        )
+        RegTool.delKey(
+            RegEnv.HKEY_CLASSES_ROOT, r'*\shell\testRegData'
+        )
+        self.assertEqual(
+            RegTool.recursion(
+                *(RegEnv.HKEY_CLASSES_ROOT, r'*\shell')
+            ).get('testRegData', {}) == writeData, False
+            , 'Data delete failed'
+        )
+
+        RegTool.writeKeyByStr(json.dumps(writeData))
+        self.assertEqual(
+            RegTool.recursion(
+                *(RegEnv.HKEY_CLASSES_ROOT, r'*\shell')
+            )['testRegData'] == writeData, True
+            , 'Data write failed'
+        )
+        RegTool.delKey(
+            RegEnv.HKEY_CLASSES_ROOT, r'*\shell\testRegData'
+        )
+        self.assertEqual(
+            RegTool.recursion(
+                *(RegEnv.HKEY_CLASSES_ROOT, r'*\shell')
+            ).get('testRegData', {}) == writeData, False
+            , 'Data delete failed'
+        )
+
+    def test_mvRegData(self):
+        writeData = {
+            '__path__': (RegEnv.HKEY_CLASSES_ROOT.value, r'*\shell\testRegData'),
+            '__val__': {
+                '': ('test reg data write', RegType.REG_SZ.value)
+            }
+            , 'child-0': {
+                '__path__': (RegEnv.HKEY_CLASSES_ROOT.value, r'*\shell\testRegData\child-0'),
+                '__val__': {
+                    '': ('test reg data child-0 write', RegType.REG_SZ.value)
+                }
+                , 'child-0-0': {
+                    '__path__': (RegEnv.HKEY_CLASSES_ROOT.value, r'*\shell\testRegData\child-0\child-0-0'),
+                    '__val__': {
+                        '': ('test reg data child-0-0 write', RegType.REG_SZ.value)
+                    }
+                }
+            }
+            , 'child-1': {
+                '__path__': (RegEnv.HKEY_CLASSES_ROOT.value, r'*\shell\testRegData\child-1'),
+                '__val__': {
+                    '': ('test reg data child-1 write', RegType.REG_SZ.value)
+                }
+            }
+        }
+
+        RegTool.writeKey(writeData)
+        self.assertEqual(
+            RegTool.recursion(
+                *(RegEnv.HKEY_CLASSES_ROOT, r'*\shell')
+            )['testRegData'] == writeData, True
+            , 'Data write failed'
+        )
+        source = (RegEnv.HKEY_CLASSES_ROOT, r'*\shell\testRegData')
+        target = (RegEnv.HKEY_CLASSES_ROOT, r'*\shell\testRegData-mv-new')
+        RegTool.mvKey(source, target)
+
+        def replacePath(regData: {}):
+            regData['__path__'] = (
+                target[0].value,
+                regData['__path__'][1].replace(source[1], target[1])
+            )
+            for key, regDataChild in regData.items():
+                if key[:2] != '__':
+                    replacePath(regDataChild)
+            return regData
+        self.assertEqual(
+            RegTool.recursion(
+                *(RegEnv.HKEY_CLASSES_ROOT, r'*\shell')
+            )['testRegData-mv-new'] == replacePath(writeData), True
+            , 'Data write failed'
+        )
