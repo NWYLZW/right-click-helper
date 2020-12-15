908b8f53afc94a845f13042ad8612572e7235c0a
yijie4188 <yijie4188@gmail.com>
2020-12-15 19:59:48 +0800

    [style] (app)

--- src/rightClickHelper/app.py
+++ src/rightClickHelper/app.py
@@ -17,12 +17,12 @@ if __name__ == '__main__':
 
         sys._excepthook = sys.excepthook
 
-        def my_exception_hook(exctype, value, traceback):
+        def myExceptionHook(exctype, value, traceback):
             print(exctype, value, traceback)
             sys._excepthook(exctype, value, traceback)
             sys.exit(1)
 
-        sys.excepthook = my_exception_hook
+        sys.excepthook = myExceptionHook
 
         try: sys.exit(APP.exec_())
         except: pass
