0a0b255c8f55e4ed04249b36b2d5fdfa76ea6b9a
yijie4188 <yijie4188@gmail.com>
2021-01-29 07:22:58 +0800

    [feat] (build) 优化打包脚本

--- build-scripts/build.py
+++ build-scripts/build.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os, shutil
+import os, toml, shutil, zipfile
+
+from tqdm import tqdm
 
 if __name__ == '__main__':
     if os.path.exists('./dist/'):
@@ -8,3 +10,24 @@ if __name__ == '__main__':
     os.system(
         'pyinstaller ./start.spec'
     )
+    config = toml.load('./pyproject.toml')['tool']['poetry']
+
+    newZipFilePath = f"./releases/{config['name']}-{config['version']}.zip"
+    latestZipFilePath = f"./releases/{config['name']}-latest.zip"
+    if not os.path.exists('./releases'):
+        os.mkdir('./releases')
+    z = zipfile.ZipFile(newZipFilePath, 'w', zipfile.ZIP_DEFLATED)
+
+    waitZipFiles = os.walk('./dist/Right Click Helper')
+    pBar = tqdm(total=sum(1 for _ in waitZipFiles), ncols=150)
+    for dirPath, dirNames, filenames in os.walk('./dist/Right Click Helper'):
+        path = dirPath.replace('./dist/Right Click Helper', '')
+        path = path and path + os.sep or ''
+        for filename in filenames:
+            z.write(os.path.join(dirPath, filename), path + filename)
+            pBar.desc = f'compressing: {filename}'
+            pBar.update(0)
+        pBar.update(1)
+    z.close()
+    print('compressed.')
+    shutil.copy(newZipFilePath, latestZipFilePath)
